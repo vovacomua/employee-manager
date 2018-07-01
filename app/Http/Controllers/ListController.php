7b05657842244762ec3d3ef1db9debbbbc796c24
Vladimir <vovacomua@bk.ru>
2018-07-01 10:16:55 +0300

    6. List all employees and order by any field

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Employee;
 
 class ListController extends Controller
 {
@@ -13,10 +14,19 @@ class ListController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
 
 	{
-    	return view('list.list');
+		$input = $request->all();
+        
+		//set default values
+        $field = $request->input('field', 'id');
+        $order = $request->input('order', 'asc');
+
+        $employees = Employee::orderBy($field, $order)->get();
+
+        return view('list.list', compact('employees'));
+
 	}
 
 }
--- resources/views/list/list.blade.php
+++ resources/views/list/list.blade.php
@@ -1,1 +1,35 @@
-<h1>Welcome to restricted area!</h1>
+@extends('layouts.master')
+
+@section('content')
+
+	<table class="table table-striped">
+
+	  <thead>
+	    <tr>
+	      <th scope="col">ID 		<a href="/restricted/?field=id&order=asc">&uarr;</a> 		<a href="/restricted/?field=id&order=desc">&darr;</a>		</th>
+	      <th scope="col">BOSS ID 	<a href="/restricted/?field=parent_id&order=asc">&uarr;</a> <a href="/restricted/?field=parent_id&order=desc">&darr;</a></th>
+	      <th scope="col">FULL NAME <a href="/restricted/?field=full_name&order=asc">&uarr;</a> <a href="/restricted/?field=full_name&order=desc">&darr;</a></th>
+	      <th scope="col">POSITION 	<a href="/restricted/?field=position&order=asc">&uarr;</a> 	<a href="/restricted/?field=position&order=desc">&darr;</a>	</th>
+	      <th scope="col">STARTED 	<a href="/restricted/?field=start_date&order=asc">&uarr;</a> <a href="/restricted/?field=start_date&order=desc">&darr;</a></th>
+	      <th scope="col">SALARY 	<a href="/restricted/?field=salary&order=asc">&uarr;</a> 	<a href="/restricted/?field=salary&order=desc">&darr;</a>	</th>
+	    </tr>
+	  </thead>
+
+    @foreach($employees as $employee)
+
+	    <tr>
+
+	        <td> {{ $employee->id }} </td>
+	        <td> {{ $employee->parent_id }} </td>
+	        <td> {{ $employee->full_name }} </td>
+	        <td> {{ $employee->position }} </td>
+	        <td> {{ $employee->start_date }} </td>
+	        <td> {{ $employee->salary }} </td>
+
+	    </tr>
+
+    @endforeach
+
+	</table>
+
+@endsection
